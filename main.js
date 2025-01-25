737166365b3c5673330e48fda389f8cd6baa3d57
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-01-25 10:10:46 -0500

    start

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: "Socks",
-      description: "Perfect for cold days",
-      onSale: true,
-      image: "./assets/images/socks_green.jpg",
-    }
+      product: 'Socks',
+      image: './assets/images/socks_blue.jpg',
+      inStock: true,
+      details: ['50% cotton', '30% wool', '20% polyester']
+  }
   },
 })
