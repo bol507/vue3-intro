ae961d8b3279b887a3270000ca69e41daa995874
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-01-24 22:37:27 -0500

    feat: add onSale

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       description: "Perfect for cold days",
+      onSale: true,
+      image: "./assets/images/socks_green.jpg",
     }
   },
 })
